cac9f7909a54f91692cbe5072b062485de2590a9
guabutian <2351180282@qq.com>
2017-12-01 11:57:55 +0800

    Junit

---
+++ Junit/src/main/java/com/cn/Demo1/test1.java
@@ -0,0 +1,15 @@
+package com.cn.Demo1;
+
+/**
+ * Created by guabutian on 2017/11/29.
+ */
+public class test1 {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int sub(int num1, int num2) {
+        return num1 - num2;
+    }
+
+}
